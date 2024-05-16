9d72f31f5c65a22849f947bb7039bf90644a4217
sreeharsha-rav <dev.sreeharsha@gmail.com>
2024-05-15 23:29:54 -0700

    e-commerce-mono init

---
+++ ecommerce-monolithic/e-commerce-mono/src/main/java/org/dev/ecommercemono/ECommerceMonoApplication.java
@@ -0,0 +1,13 @@
+package org.dev.ecommercemono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceMonoApplication.class, args);
+	}
+
+}
---
+++ ecommerce-monolithic/e-commerce-mono/src/test/java/org/dev/ecommercemono/ECommerceMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.ecommercemono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
